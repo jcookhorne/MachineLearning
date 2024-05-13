b650a00666d638cfe1b6da23f58f85bddccf2102
Joshua Cookhorne <jncookhorne@gmail.com>
2024-05-13 08:57:49 -0400

    WOrk from kelsies this weekend

--- MachineLearning/TensorFlow/01-Neural_network_regression.py
+++ MachineLearning/TensorFlow/01-Neural_network_regression.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 # print(tf.__version__)
 
 # * Create features
-X1 = np.array([-7.0, -4.0, -1.0, 2.0, 5.0, 8.0,  11.0, 14.0])
+X1 = np.array([-7.0, -4.0, -1.0, 2.0, 5.0, 8.0, 11.0, 14.0])
 # * Create labels ( usually y in lowercase)
 y1 = np.array([3.0, 6.0, 9.0, 12.0, 15.0, 18.0, 21.0, 24.0])
 
@@ -78,13 +78,17 @@ model.compile(loss=tf.keras.losses.mae,
 # print(X, y)
 # model.fit(X, y, epochs=5)
 # you cannot have a model with values that have no dimensions aka you need at least 2 and that goes for both
-model.fit(tf.expand_dims(X1, axis=-1), tf.expand_dims(y1, axis=-1), epochs=5)
+# * only 5 epochs
+# model.fit(tf.expand_dims(X1, axis=-1), tf.expand_dims(y1, axis=-1), epochs=5)
+
+# * epochs being raised to 100
+# model.fit(tf.expand_dims(X1, axis=-1), tf.expand_dims(y1, axis=-1), epochs=100)
 
 # Check out X and Y
-print(X1, y1)
-# * try and make a prediction
-print(model.predict(x=tf.cast([17.0], dtype=tf.float32)))
+# print(X1, y1)
 
+# * try and make a prediction
+# print(model.predict(x=tf.cast([17.0], dtype=tf.float32)))
 
 # * steps in modelling with tensorFLow
 # * 1) Construct or import a pretrained model relevant to your problem
@@ -105,17 +109,71 @@ print(model.predict(x=tf.cast([17.0], dtype=tf.float32)))
 # * 3) Fitting a  model - here we might fit a model for more epochs ( leave it training longer) or on more data
 # (give the model more examples to learn from).
 
+# more changes
 
-# Lets rebuild our model
 
-# 1) Create the model
-model = tf.keras.Sequential([
+# Create a new model with an extra hidden layer with 100 hidden units
+newModel = tf.keras.Sequential([
+    tf.keras.layers.Dense(50, activation=None),
     tf.keras.layers.Dense(1)
 ])
 
-# 2) Compile the model
-model.compile(loss=tf.keras.losses.mae,
-              optimizer=tf.keras.optimizers.SGD(),
-              metrics=["mae"])
-# 3 ) fit the model
-model.fit(X, y, epochs=100)
+# 2) compile the model
+newModel.compile(loss="mae",
+                 optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
+                 metrics=["mae"])
+
+# 3 fit the model
+newModel.fit(tf.expand_dims(X1, axis=-1), tf.expand_dims(y1, axis=-1), epochs=100)
+
+print(X,'\n', y)
+print(newModel.predict(x=tf.cast([17.0], dtype=tf.float32)))
+
+# although for whatever reason the data we get back is different from the video the knowledge is the same
+# a good way to improve a deep model would be to add more hidden layers
+# * another way is to increase the number of hidden units
+# * change the activation functions
+# * change the optimization function
+# * change your learning rate - most important hyperparameter of your neural network
+# * Fitting on more data
+# * fitting for longer
+
+# Evaluating a models performance
+
+# * in practice, a typical workflow you'll go through when building neural network is:
+# * build a model -> fit it -> evaluate it -> tweak a model -> fit it -> evaluate it -> tweak a model -> fit it....
+
+# * when it comes to evaluation ... there are 3 words you should memorize
+# * visualize visualize visualize
+# * its a good idea to visualize
+# * the data - what data are we working with what does it look like?
+# ** the model itself - what does our model look like?
+# * the training of a model - how does a model perform while it learns ?
+# * the predictions of the model - how do the predictions of a model line up against the ground truth
+# * (the original label)
+
+# ** make a bigger dataset
+
+X2 = tf.range(-100, 100, 4)
+
+y2 = X2 + 10
+
+print(X2, y2)
+# plt.plot(X2, y2)
+plt.scatter(X2, y2)
+plt.show()
+
+# * the 3 sets . . .
+
+# * Training set - the model learns from this data, which is typically 70-80% of the total data you have available.\
+# * Validation set - the model gets tuned on this data, which is typically 10-15% of the data available.
+#  * Test set - the model gets evaluated on this data to test what is has learned, this set is typically 10-15% of
+#  the total data available.
+# * you don't always need the validation set
+
+
+
+
+
+
+
