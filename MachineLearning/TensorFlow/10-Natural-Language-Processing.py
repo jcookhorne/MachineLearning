a21b04c795999259a13e4ca07ddbb57424807779
Joshua Cookhorne <jncookhorne@gmail.com>
2024-07-26 14:21:22 -0400

    I quit at work

---
+++ MachineLearning/TensorFlow/10-Natural-Language-Processing.py
@@ -0,0 +1,10 @@
+
+# on my home computer i did all the gpu stuff
+
+# import series of helper functions for the notebook
+from helper_functions import unzip_data
+import wget
+url = 'https://raw.githubusercontent.com/mrdbourke/tensorflow-deep-learning/main/extras/helper_functions.py'
+helper_functions = wget.download(url)
+
+wget.
